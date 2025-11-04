384d6e687ebd27cbaccbfddd331e9ba5f1dffbfd
lingyu-code <2501552703@qq.com>
2025-11-04 12:29:00 +0800

    11.4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'ipo',
+    'USER': 'root',
+    'PASSWORD': '123456',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
 
 
